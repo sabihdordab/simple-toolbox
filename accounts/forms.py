d8d8fa4808b67af343f8bca5d204ef45e0a75d6d
sabih <sabihdordab@gmail.com>
2022-08-31 20:36:54 +0430

    create a form for register in forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 
-class Register_form(forms.Form):
-    pass
+class Register_form(forms.ModelForm):
+    username = forms.TextInput()
+    email = forms.EmailField()
+    password = forms.PasswordInput()
+
+    class Meta:
+        model = User
+        fields = ['username','email','password']
+        
 
 class Login_form(forms.Form):
     pass
