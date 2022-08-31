a44bc72583d6345242050c7b08e7a1cc1eeeea63
sabih <sabihdordab@gmail.com>
2022-08-31 21:12:53 +0430

    start toolbox app

--- capstone/settings.py
+++ capstone/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'toolbox'
 ]
 AUTH_USER_MODEL = 'accounts.User'
 
