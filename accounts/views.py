b3ae96c94591107850cb6557efec56c726e0e92f
sabih <sabihdordab@gmail.com>
2022-08-31 20:52:38 +0430

    create and init register func into views.py and path it

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import User
+from .forms import *
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = Register_form(request.POST)
+        if form.is_valid:
+            try:
+                form.save()
+                return HttpResponse("ok")
+            except:
+                return HttpResponse("this username already token")
+        
+    return render(request, 'accounts/register.html',{
+        'form' : Register_form
+        })
