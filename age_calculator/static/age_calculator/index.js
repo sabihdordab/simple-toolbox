63105d6468fb565720fadf86e5ab7fb887adf3a7
sabih <sabihdordab@gmail.com>
2022-09-02 23:16:08 +0430

    fix some bug to show result of calculations

--- age_calculator/static/age_calculator/index.js
+++ age_calculator/static/age_calculator/index.js
@@ -1,32 +1,20 @@
 document.addEventListener('DOMContentLoaded',function(){
-
-    document.querySelector('#calculate').disabled = true;
-
-    document.querySelector('#birthday').onclick = function(){
-
-        if (document.querySelector('#birthday').value){
-
-            document.querySelector('#calculate').disabled = false;
-        }
-        else{
-            document.querySelector('#calculate').disabled = true;
-        }
-    }
+    document.querySelector('#message').style.display = 'none';
 })
 
 function ageCalculator(){
-    const date = document.querySelector('#birthday').value;
     document.querySelector('#calculate').addEventListener('click',function(){
         fetch('/age_calculator/calculate',{
             method : "POST" ,
             body :JSON.stringify({
-                date : date
+                date : document.querySelector('#birthday').value
             })
         })
         .then( response => response.json())
         .then( result => {
-            message = document.querySelector('#message')
-            message.innerHTML = result.message
+            message = document.querySelector('#message');
+            message.innerHTML = result.message;
+            message.style.display = 'block';
         })
     })
 }
--- age_calculator/views.py
+++ age_calculator/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
 import json
-import datetime
+from datetime import datetime
 from dateutil import relativedelta
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request, 'age_calculator/index.html')
 
-
+@csrf_exempt
 def calculate(request):
     if request.method == "POST":
-        date = json.loads(request.body).get('date')
+        data = json.loads(request.body).get('date')
         try:
-            date = datetime.datetime.strptime(date,"%d-%m-%Y")
-            now = datetime.datetime.now()
-            delta = relativedelta.relativedelta(now - date)
+            date = datetime.strptime(data.strip(),"%Y-%m-%d")
+            now = datetime.now()
+            delta = relativedelta.relativedelta(now,date)
             message = f'{delta.years} Years , {delta.months} Month , {delta.days} Days'
         except:
             message = 'failed'
-        return JsonResponse({'message':message})
+        return JsonResponse({'message': message })
     else:
         return HttpResponse(status=404)
