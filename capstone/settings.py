a0f35f119bc03d3ceb445520778a84c475464cd1
sabih <sabihdordab@gmail.com>
2022-09-02 20:16:55 +0430

    start age calculator app

--- capstone/settings.py
+++ capstone/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'toolbox',
-    'notepad'
+    'notepad',
+    'age_calculator'
 ]
 AUTH_USER_MODEL = 'accounts.User'
 
--- capstone/urls.py
+++ capstone/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('',include('toolbox.urls')),
-    path('notepad/',include('notepad.urls'))
+    path('notepad/',include('notepad.urls')),
+    path('age_calculator/',include('age_calculator.urls'))
 ]
