16b12012fa62686a983e4c060c8aec2f116c0b7c
sabih <sabihdordab@gmail.com>
2022-09-02 21:55:33 +0430

    calculate view to calculate diff of dates

--- age_calculator/urls.py
+++ age_calculator/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'age_calculator'
 urlpatterns = [
-    path('',views.index,name = 'index')
+    path('',views.index,name = 'index'),
+    path('calculate' , views.calculate , name='calculate')
 ]
--- age_calculator/views.py
+++ age_calculator/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse , JsonResponse
+import json
+import datetime
+from dateutil import relativedelta
 
 def index(request):
     return render(request, 'age_calculator/index.html')
+
+
+def calculate(request):
+    if request.method == "POST":
+        date = json.loads(request.body).get('date')
+        try:
+            date = datetime.datetime.strptime(date,"%d-%m-%Y")
+            now = datetime.datetime.now()
+            delta = relativedelta.relativedelta(now - date)
+            message = f'{delta.years} Years , {delta.months} Month , {delta.days} Days'
+        except:
+            message = 'failed'
+        return JsonResponse({'message':message})
+    else:
+        return HttpResponse(status=404)
