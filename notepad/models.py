b794c6b5c2f992c1421fa10bd97b83b711b4d3ac
sabih <sabihdordab@gmail.com>
2022-09-02 19:08:56 +0430

    create Note model

--- notepad/models.py
+++ notepad/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Note(models.Model):
+    author = models.ForeignKey(User, on_delete= models.CASCADE , related_name='notes')
+    title = models.TextField()
+    content = models.TextField()
+    date = models.DateField()
