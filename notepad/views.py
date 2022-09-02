84396f44d6d104dbd8e9269b7fe377bc78d011f0
sabih <sabihdordab@gmail.com>
2022-09-02 17:56:34 +0430

    create index view to render notepad index page and its path

--- notepad/views.py
+++ notepad/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect , HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+def index(request):
+    if request.user.is_authenticated :
+        return render(request,'notepad/index.html')
+    else:
+        return HttpResponseRedirect(reverse('login'))
