b8fe22f2c4eca246ad6d589cf93978eb19ac4d72
sabih <sabihdordab@gmail.com>
2022-09-03 22:30:30 +0430

    editNote func(js) is programming to edit note

--- notepad/static/notepad/index.js
+++ notepad/static/notepad/index.js
@@ -22,13 +22,64 @@ function showNotes(){
 }
 
 function showAddNotePage(){
-    document.querySelector('#content').value = ""
-    document.querySelector('#title').value = ""
+    document.querySelector('#content').value = "";
+    document.querySelector('#title').value = "";
     document.querySelector('#notes-div').style.display= 'none';
     document.querySelector('#new-note-div').style.display= 'block';
 }
 
 
 function editNote(id){
-    document.querySelector('#note-div').style.display = 'none'
+    document.querySelector('#content').value = ""
+    document.querySelector('#title').value = ""
+    document.querySelector('#notes-div').style.display= 'none';
+    document.querySelector('#new-note-div').style.display= 'block';
+}
+
+function editNote(id){
+    document.querySelector('#note-div').style.display = 'none';
+    const title = document.querySelector('#note-title');
+    const content = document.querySelector('#note-content');
+    let editDiv = document.querySelector('#edit-note-div');
+    const br = document.createElement('br');
+    let titleLabel = document.createElement('label');
+    titleLabel.innerHTML = "title";
+    let contentLabel = document.createElement('label');
+    contentLabel.innerHTML = "content";
+    let div = document.createElement('div');
+    let input = document.createElement('INPUT');
+    input.setAttribute("type", "text");
+    input.id = 'title-field';
+    let textarea= document.createElement('textarea');
+    textarea.setAttribute("rows",20);
+    textarea.id = 'content-field';
+    let submit = document.createElement('button');
+    textarea.className = 'form-control';
+    input.className = 'form-control';
+    input.value = title.innerHTML;
+    textarea.value = content.innerHTML;
+    textarea.style.width = '100%';
+    title.style.width = '100%';
+    submit.innerHTML = "Submit";
+    submit.className = 'btn btn-primary';
+    div.appendChild(titleLabel)
+    div.appendChild(input);
+    div.appendChild(contentLabel);
+    div.appendChild(textarea);
+    div.appendChild(br);
+    div.appendChild(submit);
+    div.style.margin = '20px';
+    editDiv.appendChild(div);
+
+
+    submit.addEventListener('click',function(){
+        if (!document.querySelector('#content-field').value.trim())
+        {
+            alert("Note content can't be None");
+        }
+        else{
+            
+            editDiv.removeChild(div);
+        }
+    })
 }
