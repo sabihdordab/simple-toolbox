56e6177a2bec10389a4343d55a1b0c24f32e507d
sabih <sabihdordab@gmail.com>
2022-09-03 13:28:49 +0430

    using js to footwork:))) between two div

--- notepad/static/notepad/index.js
+++ notepad/static/notepad/index.js
@@ -1,12 +1,27 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#add-note-btn').disabled = true;
-    document.querySelector('#content').onkeyup = function(){
-        if (document.querySelector('#content').value.trim()){
-            document.querySelector('#add-note-btn').disabled = false;
-        }
-        else{
-            document.querySelector('#add-note-btn').disabled = true;
-        }
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#notes-btn').addEventListener('click',showNotes);
+    document.querySelector('#add-note-btn').addEventListener('click',showAddNotePage);
+    showNotes();
 })
 
+function showNotes(){
+    document.querySelector('#new-note-div').style.display= 'none';
+    document.querySelector('#notes-div').style.display= 'block';
+}
+
+function showAddNotePage(){
+    
+    document.querySelector('#submit-note').disabled = true;
+    
+    document.querySelector('#content').onkeyup = function(){
+        if (document.querySelector('#content').value.trim()){
+            document.querySelector('#submit-note-').disabled = false;
+        }
+        else{
+            document.querySelector('#submit-note').disabled = true;
+        }
+    }
+    document.querySelector('#notes-div').style.display= 'none';
+    document.querySelector('#new-note-div').style.display= 'block';
+    document.querySelector('#new-note-div').className = 'container';
+}
