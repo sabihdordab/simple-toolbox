cb5badf7aef4378ce4c1d1e27656cb26a02a97a0
sabih <sabihdordab@gmail.com>
2022-09-03 14:29:08 +0430

    add_note view programmed to save note and return the note page

--- notepad/views.py
+++ notepad/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect , HttpResponse
 from django.urls import reverse
+from .models import Note
+import datetime
+from markdown2 import Markdown
 
 def index(request):
     if request.user.is_authenticated :
@@ -9,7 +12,21 @@ def index(request):
         return HttpResponseRedirect(reverse('login'))
 
 def add_note(request):
+    user = request.user
+    if not user.is_authenticated :
+        return HttpResponseRedirect(reverse('login'))
+    
     if request.method == "POST":
-        pass
+        title = request.POST['title']
+        if not title:
+            title = datetime.datetime.now().strftime('%Y-%m-%d')
+        content = request.POST['content']
+        note = Note(author=user,title=title,content=content,date=datetime.datetime.now())
+        note.save()
+        HttpResponseRedirect(reverse('notepad:note',args=(id,)))
     else:
         HttpResponse(status=405)
+
+
+def note_page(request,id):
+    pass
